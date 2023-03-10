fb9d0af10360eec9ddab46d5f6134eaca516f13a
student_2333 <lgc2333@126.com>
2023-03-10 20:04:53 +0800

    变高级一点

--- nonebot_plugin_random_stereotypes/__init__.py
+++ nonebot_plugin_random_stereotypes/__init__.py
@@ -1,8 +1,9 @@
 import random
 
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Bot, Event, Message
-from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, MessageEvent
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg, Arg
 from nonebot.plugin import PluginMetadata
 
 from .data import DATA
@@ -17,11 +18,16 @@ catch_str = on_command("发病")
 
 
 @catch_str.handle()
-async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
-    content = msg.extract_plain_text()
+async def _(matcher: Matcher, arg: Message = CommandArg()):
+    if arg.extract_plain_text().strip():
+        matcher.set_arg("target", arg)
 
-    random_num = random.randint(1, len(DATA)) - 1
 
-    msg = DATA[random_num].format(target_name=content)
+@catch_str.got("target", "你要对哪个人发病呢？")
+async def _(event: MessageEvent, matcher: Matcher, target: Message = Arg("target")):
+    target_str = target.extract_plain_text().strip()
+    if not target_str:
+        await matcher.reject("你发的消息中没有文本，请重新输入！")
 
-    await catch_str.finish(Message(f"{msg}"), at_sender=True)
+    msg = random.choice(DATA).format(target_name=target_str)
+    await matcher.finish(MessageSegment.reply(event.message_id) + msg)
