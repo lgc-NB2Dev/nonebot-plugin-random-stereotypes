88e5861d5f6edd6aed0545f53c5ae171966c6fb8
student_2333 <lgc2333@126.com>
2023-03-10 19:55:22 +0800

    add metadata

--- nonebot_plugin_random_stereotypes/__init__.py
+++ nonebot_plugin_random_stereotypes/__init__.py
@@ -1,11 +1,17 @@
-from nonebot.adapters.onebot.v11 import Message
-from nonebot import on_command
-from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11 import Bot, Event
-# import nonebot
 import random
 
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Bot, Event, Message
+from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
+
 from .data import DATA
+
+__plugin_meta__ = PluginMetadata(
+    name="发病语录",
+    description="随机返回一条发病语录",
+    usage="命令：发病 [发病对象]\n例如：发病 测试",
+)
 
 catch_str = on_command("发病")
 
@@ -18,4 +24,4 @@ async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
 
     msg = DATA[random_num].format(target_name=content)
 
-    await catch_str.finish(Message(f'{msg}'), at_sender=True)
+    await catch_str.finish(Message(f"{msg}"), at_sender=True)
