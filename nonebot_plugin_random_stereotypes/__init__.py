97230286fc82edd61961db19c6d0d18507c1bf49
LeoWrDeom <493269505@qq.com>
2024-08-24 09:49:14 +0800

    __init__.py

--- nonebot_plugin_random_stereotypes/__init__.py
+++ nonebot_plugin_random_stereotypes/__init__.py
@@ -1,6 +1,6 @@
 import random
 from nonebot.params import Depends
-from nonebot.adapters.onebot.v11 import Message, MessageEvent
+from nonebot.adapters.onebot.v11 import Bot,Message, MessageEvent , MessageSegment, Message, GroupMessageEvent
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg, Arg
 from nonebot.plugin import PluginMetadata
@@ -19,16 +19,56 @@ catch_str = create_matcher()
 
 
 @catch_str.handle(parameterless=[Depends(check_CD)])
-async def _(matcher: Matcher, arg: Message = CommandArg()):
-    if arg.extract_plain_text().strip():
-        matcher.set_arg("target", arg)
-
+async def _(matcher: Matcher,bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
+    msg = event.get_message()
+    if "at" in msg:
+        gid = event.group_id  # 获取群号
+        uid = event.user_id
+        qid = await get_at(event)
+        target_str = ""
+        print(qid)
+        if not qid:
+                if arg.extract_plain_text().strip():
+                    matcher.set_arg("target", arg)
+        else:
+            await bot.call_api('get_group_member_list', group_id=gid)
+            member_info = await bot.get_group_member_info(group_id=gid, user_id=qid)
+            card_name = member_info['card'] if member_info['card'] else member_info['nickname']
+            target_str = card_name
+            print(qid)
+            await bot.call_api('get_group_member_list', group_id=gid)
+            member_info = await bot.get_group_member_info(group_id=gid, user_id=uid)
+            card_name = member_info['card'] if member_info['card'] else member_info['nickname']
+            msg1 = f"{card_name}说：\n-----------\n"
+            msg = msg1 + random.choice(DATA).format(target_name=target_str)
+            await matcher.finish(message=msg, reply_message=True)
+    else:
+        if arg.extract_plain_text().strip():
+            matcher.set_arg("target", arg)
 
 @catch_str.got("target", "你要对哪个人发病呢？")
-async def _(matcher: Matcher, target: Message = Arg("target")):
+async def _(matcher: Matcher,bot: Bot, event: GroupMessageEvent, target: Message = Arg("target")):
     target_str = target.extract_plain_text().strip()
+    gid = event.group_id  # 获取群号
+    uid = event.user_id
+    qid = await get_at(event)
+    print(qid)
     if not target_str:
-        await matcher.reject("你发的消息中没有文本，请重新输入！")
+        await bot.call_api('get_group_member_list', group_id=gid)
+        member_info = await bot.get_group_member_info(group_id=gid, user_id=qid)
+        card_name = member_info['card'] if member_info['card'] else member_info['nickname']
+        target_str = card_name
+        print(target_str)
+        if not card_name:
+            await matcher.reject("你发的消息中没有文本，请重新输入！")
 
     msg = random.choice(DATA).format(target_name=target_str)
     await matcher.finish(message=msg, reply_message=True)
+
+async def get_at(event: GroupMessageEvent) -> int:
+    """获取被艾特用户 ID"""
+    msg = event.get_message()
+    for msg_seg in msg:
+        if msg_seg.type == "at":
+            return -1 if msg_seg.data["qq"] == "all" else int(msg_seg.data["qq"])
+    return -1
